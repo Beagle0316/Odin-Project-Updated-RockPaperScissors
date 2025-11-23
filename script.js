f03b080ade1565b00d83aa899302dcd274e9758f
Beagle0316 <twynsta.beagle@gmail.com>
2025-11-23 21:11:59 +0800

    Add text for displaying running score

--- script.js
+++ script.js
@@ -44,79 +44,68 @@ function getHumanChoice(){
 // FUNCTION playRound
 function playRound(humanChoice, computerChoice){
     const container = document.querySelector(".textContainer");
-    const text = document.createElement("p");
+    const text = document.createElement("p"); 
+    let statusMessage;
+
     switch (humanChoice){
         case "ROCK":
             if (computerChoice === "ROCK"){
-                text.textContent = "Tie! Rock equals Rock";
+                statusMessage = "Tie! Rock equals Rock";
             }
             else if (computerChoice === "PAPER"){
-                text.textContent = "You lose! Paper beats Rock";
-                ncomputerScore++;
+                statusMessage = "You lose! Paper beats Rock";
+                ++ncomputerScore;
+
             }
             else{
-                text.textContent = "You win! Rock beats Scissors";
-                nhumanScore++;
+                statusMessage = "You win! Rock beats Scissors";
+                ++nhumanScore;
             }
             break;
         case "PAPER":
             if (computerChoice === "ROCK"){
-                text.textContent = "You win! Paper beats Rock";
-                nhumanScore++;
+                statusMessage = "You win! Paper beats Rock";
+                ++nhumanScore;
             }
             else if (computerChoice === "PAPER"){
-                text.textContent = "Tie! Paper equals Paper";
+                statusMessage = "Tie! Paper equals Paper";
 
             }
             else{
-                text.textContent = "You Lose! Scissors beats Paper";
-                ncomputerScore++;
+                statusMessage = "You Lose! Scissors beats Paper";
+                ++ncomputerScore;
             }
             break;
         case "SCISSORS":
             if (computerChoice === "ROCK"){
-                text.textContent = "You lose! Rock beats Scissors";
-                ncomputerScore++;
+                statusMessage = "You lose! Rock beats Scissors";
+                ++ncomputerScore;
             }
             else if(computerChoice === "PAPER"){
-                text.textContent = "You win! Scissors beats Paper";
-                nhumanScore++;
+                statusMessage = "You win! Scissors beats Paper";
+                ++nhumanScore;
             }
             else{
-                text.textContent = "Tie! Scissors equals Scissors";
+                statusMessage = "Tie! Scissors equals Scissors";
             }
             break;
         default:
-            text.textContent = "Invalid input! Point goes to computer!";
-            ncomputerScore++;
+            statusMessage = "Invalid input! Point goes to computer!";
+            ++ncomputerScore;
     }
+    const scoreMessage = `Human: ${nhumanScore} Computer: ${ncomputerScore}`;
+    text.innerHTML = statusMessage + "<br>" + scoreMessage;
     container.appendChild(text);
 }
 
 // FUNCTION playGame
 function playGame(){
     const buttons = document.querySelectorAll("button");
-    
     buttons.forEach((button) => {
         button.addEventListener("click", () =>{
             playRound(button.className, getComputerChoice());
         });
     });
-    
-
-
-    /*
-    // SEQUENCE declare the winner of the game
-    if (nhumanScore > ncomputerScore){
-        console.log("Human Player Wins!");
-    }
-    else if(ncomputerScore > nhumanScore){
-        console.log("Computer Player Wins!");
-    }
-    else{
-        console.log("It is a tie! Refresh and play again!");
-    }
-    */
 }
 // DECLARE global variables
 let nhumanScore = 0;
@@ -124,4 +113,16 @@ let ncomputerScore = 0;
 
 // SEQUENCE play the game!
 playGame();
+
+
+// SEQUENCE declare the winner of the game
+if (nhumanScore > ncomputerScore){
+    console.log("Human Player Wins!");
+}
+else if(ncomputerScore > nhumanScore){
+    console.log("Computer Player Wins!");
+}
+else{
+    console.log("It is a tie! Refresh and play again!");
+}
 
